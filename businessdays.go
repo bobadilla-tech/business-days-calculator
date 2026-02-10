7be0b34c20e5aec1b76b39da6ed15df21ef0b22b
Leonardo <leonardo.estacio.h@gmail.com>
2026-02-10 10:23:21 -0500

    fix: optimize business days calculation

--- businessdays.go
+++ businessdays.go
@@ -10,16 +10,18 @@ func CalculateBusinessDays(start, end time.Time) int {
 	start = time.Date(start.Year(), start.Month(), start.Day(), 0, 0, 0, 0, start.Location())
 	end = time.Date(end.Year(), end.Month(), end.Day(), 0, 0, 0, 0, end.Location())
 
-	totalDays := int(end.Sub(start).Hours()/24) + 1
+	totalDays := int(end.Sub(start)/(24*time.Hour)) + 1
 
 	fullWeeks := totalDays / 7
 	remainingDays := totalDays % 7
 
 	businessDays := fullWeeks * 5
 
+	weekday := int(start.Weekday())
+
 	for i := range remainingDays {
-		day := start.AddDate(0, 0, fullWeeks*7+i).Weekday()
-		if day != time.Saturday && day != time.Sunday {
+		day := (weekday + i) % 7
+		if day != int(time.Saturday) && day != int(time.Sunday) {
 			businessDays++
 		}
 	}
